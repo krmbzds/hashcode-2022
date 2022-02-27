1fde1a2b81a0ef3f05caf2839542845d867e8e82
Kerem Bozdas <krmbzds.github@gmail.com>
2022-02-27 19:09:48 +0300

    Create gem scaffold

---
+++ lib/hashcode/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hashcode
+  VERSION = "0.1.0"
+end
---
+++ spec/hashcode_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Hashcode do
+  it "has a version number" do
+    expect(Hashcode::VERSION).not_to be nil
+  end
+end
